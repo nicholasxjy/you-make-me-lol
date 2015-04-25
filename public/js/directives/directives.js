d71186e8c8d895156725a17d56bab61ffc58b1f0
nicholasxjy <yitaqi2011@gmail.com>
2015-04-25 21:17:07 +0800

    fix some

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -779,7 +779,7 @@
             <div class="comment-content">\
               <div class="top">\
                 <a class="comment-user-name" ui-sref="user({name: comment.creator.name})">{{comment.creator.name}}</a>\
-                <span class="comment-time">{{comment.createdAt | sinceTime}}</span>\
+                <span class="comment-time" ng-cool-tooltip="{{comment.createdAt | date: \'yyyy-MM-dd HH:mm\'}}">{{comment.createdAt | sinceTime}}</span>\
                 <i class="ti-trash pull-right" ng-if="comment.creator._id == current_user._id" ng-click="deleteComment(comment)"></i>\
               </div>\
               <div class="comment-words" ng-bind-html="comment.content">\
